7b8809b4638b2ae32b9bb48f539f1f02384f381e
Dmytro Kuzminskyi <Dimka333@users.noreply.github.com>
2021-01-13 01:12:20 +0200

    ДЗ js №5

---
+++ js/task-number_3.js
@@ -0,0 +1,29 @@
+const Storage = function (item) {
+    const items = storagee;
+    console.log(items);
+}
+
+
+
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+Storage.prototype.getItems = function () {
+    console.log(items);
+};
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
