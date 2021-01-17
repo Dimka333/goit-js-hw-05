c527a4ba348c2afc46be9cc2f191d707bf7b42fc
Dmytro Kuzminskyi <Dimka333@users.noreply.github.com>
2021-01-17 21:50:35 +0200

    ДЗ js №5

--- js/task-number_4.js
+++ js/task-number_4.js
@@ -1,38 +1,37 @@
 class StringBuilder {
     constructor(value) {
-        this._value = value;    
+        this._value = value;        
     }
-    get value() {
+    get value() {   
         return this._value;
-    };
+    }
    
-    append(value) {
-        this._value.push(value);
+    append(str) {
+     this._value = this._value + str;
+    
        
-    };
+    }
 
-    prepend(value) {
-        this._value.unshift(value);
+    prepend(str) {
+       this._value = str + this._value;  
         
-    };
+    }
 
-    pad(value) {
-        this._value.push(value);
-        this._value.unshift(value);
+    pad(str) {
+     this._value = str+ this._value + str;
 
     }
 
+};
 
-}
+const builder = new StringBuilder('.');
 
-
-const builder = new StringBuilder(['.']);
 
 builder.append('^');
-console.log(builder.value.join('')); // '.^'
+console.log(builder.value); // '.^'
 
 builder.prepend('^');
-console.log(builder.value.join('')); // '^.^'
+console.log(builder.value); // '^.^'
 
 builder.pad('=');
-console.log(builder.value.join('')); // '=^.^='
+console.log(builder.value); // '=^.^='
